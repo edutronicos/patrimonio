9c37612207419b3d260835f51e771278298791c9
edutronicos <amgp@ymail.com>
2024-02-21 17:29:21 -0300

    Atualizado pagina Consulta

--- resources/views/consulta/consulta.blade.php
+++ resources/views/consulta/consulta.blade.php
@@ -1,5 +1,49 @@
 <x-app-layout>
 <div class="md:max-w-full mx-auto p-6 lg:p-8">
+    <div class="grid grid-cols-1 content-center gap-6 lg:gap-8 scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+    
+        <h1>Filtrar</h1>
+
+        <form action="" method="get" class="md:grid md:grid-cols-4">
+            <div>
+                <x-input-label for="descricao" :value="__('Descrição')" />
+                <x-text-input id="descricao" class="block mt-1 w-72" type="search" name="descricao" />
+            </div>
+
+            <div>
+                <x-input-label for="setor" :value="__('Setor')" />
+                <select class="block mt-1 w-72 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" name="setor" id="setor">
+                    <option value="Diretoria">Diretoria</option>
+                    <option value="Financeiro">Financeiro</option>
+                    <option value="Informatica">Informatica</option>
+                    <option value="Juridico">Jurídico</option>
+                    <option value="Licitacao">Licitacao</option>
+                    <option value="Recepcao">Recepção</option>
+                    <option value="Recrutamento">Recrutamento</option>
+                    <option value="RH">RH</option>
+                </select>
+            </div>
+
+            <div>
+                <x-input-label for="categoria" :value="__('Categoria')" />
+                <select class="block mt-1 w-72 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" name="categoria" id="categoria">
+                    <option value="Computadores">Computadores</option>
+                    <option value="Moveis">Móveis</option>
+                    <option value="Notebooks">Notebooks</option>
+                    <option value="Perifericos">Periféricos</option>
+                    <option value="Utensilios">Utensilios</option>
+                    <option value="Veiculos">Veículos</option>
+                </select>
+            </div>
+            
+            <div>
+            
+                <x-primary-button class="mt-7 w-32">{{ __('Filtrar') }}</x-primary-button>
+            </div>
+            
+        </form>
+    </div>
+
     <div class="flex justify-center">
         <h1>Controle de Patrimonio</h1>   
     </div>
@@ -37,6 +81,12 @@
             @endforeach
             </tbody>
         </table>
+        <div>
+            <div>
+            <x-input-label for="categoria" :value="__('Nome')" />
+            <x-text-input id="descricao" class="block mt-1 w-72 border-none" type="text" name="descricao" disabled placeholder="Nomenclatura"/>
+            </div>
+        </div>
     </div>
 
 </div>
